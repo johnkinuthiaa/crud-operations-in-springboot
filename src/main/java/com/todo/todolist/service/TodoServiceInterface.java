843bc93d879433e35a6a5a72f8da8cd0cfa68367
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-09-30 00:32:07 +0300

    initial commit

--- src/main/java/com/todo/todolist/service/TodoServiceInterface.java
+++ src/main/java/com/todo/todolist/service/TodoServiceInterface.java
@@ -2,10 +2,13 @@ package com.todo.todolist.service;
 
 import com.todo.todolist.model.Todos;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TodoServiceInterface {
     List<Todos> getAllTodos();
+    List<Todos> getTodoByName(String name);
+    List<Todos> getTodoByDate(LocalDate date);
     Todos createNewTodo(Todos todo,Long id);
     Todos getTodoById(Long id);
     Todos updateTodoList(Todos todo, Long id);
